11cff84bf2710f2e3af3b042d92c290a73b53547
Jason Butwell <jasonbutwell@gmail.com>
2016-11-09 16:58:19 +0000

    Removed user 'Barry'.

--- app/src/main/java/com/jasonbutwell/versioncontroldemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/versioncontroldemo/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String user = "Bob";
-        String anotherUser = "Barry";
     }
 }
