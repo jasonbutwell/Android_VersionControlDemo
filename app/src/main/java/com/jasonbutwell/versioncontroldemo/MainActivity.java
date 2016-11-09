36385f56dcf5ca4a362ac451c6d1a66bf4e3a44d
Jason Butwell <jasonbutwell@gmail.com>
2016-11-09 16:41:19 +0000

    Added user 'Barry'.

--- app/src/main/java/com/jasonbutwell/versioncontroldemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/versioncontroldemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String user = "Bob";
+        String anotherUser = "Barry";
     }
 }
